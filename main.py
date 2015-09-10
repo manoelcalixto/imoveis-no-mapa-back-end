cbb1109bfaaf3f35c558572dda4dc740c0f74ffb
unknown <manoelcalixto@eus_063953.santaclara.com.br>
2015-09-10 12:11:30 -0300

    Busca de cidades, estados e bairros

--- main.py
+++ main.py
@@ -12,16 +12,12 @@ CORS(app)
 connection = MongoClient('localhost', 27017)
 db = connection.mydb
 imoveis = db.imoveis
+estados = db.estados
+cidades = db.cidades
+bairros = db.bairros
 
 def toJson(data):
 	return json.dumps(data, default=json_util.default)
-
-
-@app.route('/buscarImoveisPorFiltro', methods=['POST'])
-def buscarImoveisPorFiltro():
-    req_json = request.get_json()
-    print (req_json)
-    return "oi"
 
 @app.route('/buscarImoveisPorGeoLocalizacao', methods=['POST'])
 def buscarImoveisPorGeoLocalizacao():
@@ -32,6 +28,66 @@ def buscarImoveisPorGeoLocalizacao():
 		{"tipo" : {"$in": req_json['tipos']}},
 		{"operacao" : req_json['operacao']}
 		]})
+
+	json_results = []
+	for result in results:
+		json_results.append(result)
+
+	resp = Response(
+		response=toJson(json_results), status=200, mimetype="application/json")
+	return resp
+
+@app.route('/buscarImoveisPorFiltro', methods=['POST'])
+def buscarImoveisPorFiltro():
+	req_json = request.get_json()
+	print (req_json)
+	results = imoveis.find({"$and":[
+		{"loc" : {"$within" : {"$box" : req_json['location']}}},
+		{"tipo" : {"$in": req_json['tipos']}},
+		{"operacao" : req_json['operacao']}
+		]})
+
+	json_results = []
+	for result in results:
+		json_results.append(result)
+
+	resp = Response(
+		response=toJson(json_results), status=200, mimetype="application/json")
+	return resp
+
+@app.route('/buscarEstados', methods=['POST'])
+def buscarEstados():
+	req_json = request.get_json()
+	print (req_json)
+	results = estados.find()
+
+	json_results = []
+	for result in results:
+		json_results.append(result)
+
+	resp = Response(
+		response=toJson(json_results), status=200, mimetype="application/json")
+	return resp
+
+@app.route('/buscarCidadesPorEstado', methods=['POST'])
+def buscarCidadesPorEstado():
+	req_json = request.get_json()
+	print (req_json)
+	results = cidades.find({"estado" : req_json['estado']})
+
+	json_results = []
+	for result in results:
+		json_results.append(result)
+
+	resp = Response(
+		response=toJson(json_results), status=200, mimetype="application/json")
+	return resp
+
+@app.route('/buscarBairrosPorEstadoECidade', methods=['POST'])
+def buscarBairrosPorEstadoECidade():
+	req_json = request.get_json()
+	print (req_json)
+	results = bairros.find({"cidade" : req_json['cidade']})
 
 	json_results = []
 	for result in results:
